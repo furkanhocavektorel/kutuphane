e06b05b4b47ba30ca94b7e7d3f5ac396bfdda76b
furkanhocavektorel <furkanhocavektorel@gmail.com>
2024-08-17 14:42:14 +0300

    first commit

---
+++ src/test/java/com/vektorel/kutuphane/KutuphaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vektorel.kutuphane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KutuphaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
